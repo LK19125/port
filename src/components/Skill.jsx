de265386613c052769af73c9b5c55f9718494002
LK19125 <logukumaran19@gmail.com>
2026-02-26 18:57:59 +0530

    Update Skill.jsx

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -3,7 +3,7 @@ import SkillDonut from "./SkillDonut";
 const skills = [
   { skill: "HTML", percent: 90 },
   { skill: "CSS", percent: 85 },
-  { skill: "JavaScript", percent: 50 },
+  { skill: "JavaScript", percent: 75 },
   { skill: "React", percent: 45 },
   { skill: "Python", percent: 75 },
 ];
@@ -11,12 +11,14 @@ const skills = [
 const Skills = () => {
   return (
     <>
-      <h1 className="title">Skills</h1>
-      <div className="skill-place align">
-        <div className="skill">
-          {skills.map((s, idx) => (
-            <SkillDonut key={idx} skill={s.skill} percent={s.percent} />
-          ))}
+      <div>
+        <h1 className="title">Skills</h1>
+        <div className="skill-place align">
+          <div className="skill">
+            {skills.map((s, idx) => (
+              <SkillDonut key={idx} skill={s.skill} percent={s.percent} />
+            ))}
+          </div>
         </div>
       </div>
     </>
